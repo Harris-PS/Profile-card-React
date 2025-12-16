ec2f4ccf588afc0a9c30e932398813335f797913
Harris-PS <psharris2002@gmail.com>
2025-12-16 11:52:49 +0530

    Created reusable profilecard components using  props

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -1,13 +1,11 @@
 function Profile(props){
   return (
-
     <div className="student">
       <p>Name: {props.name}</p>
       <p>Age: {props.age}</p>
       <p>Role: {props.work}</p>
     </div>
   )
-
 }
 
 export default Profile

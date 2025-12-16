c6e41e7703bbaa02628080602c5b6ba533faae6d
Harris-PS <psharris2002@gmail.com>
2025-12-16 11:38:22 +0530

    initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Profile from './Profile.jsx'
+
+function App() {
+
+  return (
+   <>
+      <Profile name="Jason" age={23} work="Frontend Developer" />
+      <Profile name="Jhon" age={25} work="Backend Developer" />
+      <Profile name="Adam" age={30} work="FullStack Developer" />
+   </>
+  );
+}
+
+export default App
---
+++ src/Profile.jsx
@@ -0,0 +1,13 @@
+function Profile(props){
+  return (
+
+    <div className="student">
+      <p>Name: {props.name}</p>
+      <p>Age: {props.age}</p>
+      <p>Role: {props.work}</p>
+    </div>
+  )
+
+}
+
+export default Profile
